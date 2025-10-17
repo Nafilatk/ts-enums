0f3952cfa1d19d2a25f505f8432ae3bb4418a96d
nafila <nafilatk7.com>
2025-10-17 15:45:10 +0530

    enums

---
+++ enums.js
@@ -0,0 +1,8 @@
+var Direction;
+(function (Direction) {
+    Direction["Up"] = "UP";
+    Direction["Down"] = "DOWN";
+    Direction["Left"] = "LEFT";
+    Direction["Right"] = "RIGHT";
+})(Direction || (Direction = {}));
+console.log(Direction.Left);
